d31481def0b033acfa12a98eedf30a0166b00ed4
Manas Kumar <manaskumar2808@gmail.com>
2022-01-29 00:16:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const cors = require('cors');
+
+const io = socketIO(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"],
+    }
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send("Server is running!");
+});
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callended');
+    });
+
+    socket.on('calluser', ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit('calluser', {
+            signal: signalData,
+            from,
+            name,
+        });
+    });
+
+    socket.on('answercall', (data) => {
+        io.to(data.to).emit('callaccepted', data.signal);
+    });
+});
+
+server.listen(PORT, () => console.log(`Server listening on port : ${PORT}`));
